46e00a470479970231bc9c1b8ab6d8398079f50c
OWQ <qaqiowo@gmail.com>
2021-12-03 15:18:06 +0800

    Site updated: 2021-12-03 15:18:05

--- js/src/funny_title.js
+++ js/src/funny_title.js
@@ -1,16 +1,16 @@
 /*离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
-window.onload = function () {
+window.onload = function() {
     let OriginTitle = document.title;
     let titleTime;
-    document.addEventListener('visibilitychange', function () {
-      if (document.hidden) {
-        document.title = '╭(°A°`)╮ 页面崩溃啦~ ' + OriginTitle;
-        clearTimeout(titleTime);
-      } else {
-        document.title = '(ฅ>ω<*ฅ) 噫又好了鸭~ ' + OriginTitle;
-        titleTime = setTimeout(function () {
-          document.title = OriginTitle;
-        }, 1500);
-      }
+    document.addEventListener('visibilitychange', function() {
+        if (document.hidden) {
+            document.title = '╭(°A°`)╮ ' + OriginTitle;
+            clearTimeout(titleTime);
+        } else {
+            document.title = '(ฅ>ω<*ฅ) ' + OriginTitle;
+        }
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 1800);
     });
-  };
+};
